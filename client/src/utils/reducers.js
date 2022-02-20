3841e15a26bce906dee213b57ff6b86511ab806c
nblake707 <33844526+nblake707@users.noreply.github.com>
2022-02-19 20:31:57 -0500

    added 2 shopping cart reducers and tests

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -2,8 +2,14 @@ import { useReducer } from 'react';
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
-    UPDATE_CURRENT_CATEGORY
-} from "./actions";
+    UPDATE_CURRENT_CATEGORY,
+    ADD_TO_CART,
+    ADD_MULTIPLE_TO_CART,
+    REMOVE_FROM_CART,
+    UPDATE_CART_QUANTITY,
+    CLEAR_CART,
+    TOGGLE_CART
+  } from './actions';
 
 export const reducer = (state, action) => {
     switch(action.type) {
@@ -23,6 +29,17 @@ export const reducer = (state, action) => {
             return {
                 ...state,
                 currentCategory: action.currentCategory
+            }
+        case ADD_TO_CART:
+            return {
+                ...state, // preserves everyting else on state
+                cartOpen: true,
+                cart: [...state.cart, action.product]
+            }
+        case ADD_MULTIPLE_TO_CART:
+            return {
+                ...state,
+                cart: [...state.cart, ...action.products],
             }
         default:
             // if no match for above action type then return current state
